c28869676d306a21f4cf1f31d11fdc9ceaa33dac
jason-nordheim <jason.nordheim@gmail.com>
2020-04-22 17:47:31 -0400

    added post as having many comments

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,4 @@
 class Post < ApplicationRecord
   validates_presence_of :title 
+  has_many :comments
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # nesting routes as comments are children of posts 
+  # this will create all the routes from posts to thier comments 
   resources :posts do 
     resources :comments
   end 
